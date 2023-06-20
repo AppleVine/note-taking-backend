886ceeedb08e0ef25b8de2b7e82ead00e30ac68f
Jakeb <Jakebco99@icloud.com>
2023-06-20 16:18:08 +1000

    mid way through sat

--- src/controllers/notes_controller.js
+++ src/controllers/notes_controller.js
@@ -1,9 +1,22 @@
+const { response } = require('express')
 const Note = require('../models/notes')
 
 const getNotes = async (request, response) =>{
 
     let notes = await Note.find()
     response.send(notes)
+}
+
+const getNote = async (request, response) => {
+    let note = await Note.findById(request.params.id)
+                        .catch(error => {
+                            console.log("Error: \n" + error)
+                            response.status(404)
+                            response.json({
+                                error: "id not found in the database"
+                            })
+                        })
+    response.send(note)
 }
 
 const createNote = async (request, response) => {
@@ -19,6 +32,21 @@ const createNote = async (request, response) => {
     response.json({note: newNote})
 }
 
+const updateNote = async (request, response) => {
+    // setting new to true makes you recieve the updated note. 
+    let updatedNote = await Note.findByIdAndUpdate(request.params.id, request.body, {new: true})
+                                .catch(error =>{
+                                    console.log("An error occured: \n" + error)
+                                })
+    // if we can find the note, we will update it.
+    if (updatedNote) {
+        response.send(updatedNote)
+    } else { // if the id doesn't exist, the note will be undefined and will return an error message.
+        response.json({error: "id not found."})
+    }
+    
+}
+
 const deleteAllNotes = async (request, response) => {
     await Note.deleteMany({})
     response.json({
@@ -26,4 +54,18 @@ const deleteAllNotes = async (request, response) => {
     })
 }
 
-module.exports = {getNotes, createNote, deleteAllNotes}
+const deleteNote = async (request, response) => {
+    await Note.findByIdAndDelete(request.params.id)
+                .catch(error => {
+                console.log("Error: \n" + error)
+                response.status(404)
+                response.json({
+                    error: "id not found in the database"
+        })
+    })
+    response.json({
+        "message": "Note Deleted."
+    })
+}
+
+module.exports = {getNotes, createNote, deleteAllNotes, getNote, deleteNote, updateNote}
--- src/routes/notes_routes.js
+++ src/routes/notes_routes.js
@@ -1,34 +1,17 @@
 const express = require('express')
 const notesRouter = express.Router();
-const {getNotes, createNote, deleteAllNotes} = require('../controllers/notes_controller')
-
-notesRouter.get("/notes", (request, response) => {
-    response.json(
-        {"message": "The list of notes goes here."}
-    )
-})
-
-notesRouter.post("/notes", (request, response) => {
-    response.json(
-        {
-            "note": {
-                "id": 1,
-                "title": "Welcome to the note taker",
-                "description": "Make your notes here",
-                "isCompleted": false,
-                "dueDate": new Date().setDate(new Date().getDate() + 1 ),
-                "createdAtDate": Date.now()
-            }
-        }
-    )
-})
-
+const {getNotes, createNote, deleteAllNotes, getNote, deleteNote, updateNote} = require('../controllers/notes_controller')
 
 notesRouter.get("/", getNotes)
 
+notesRouter.get("/:id", getNote)
 
 notesRouter.post("/", createNote)
 
 notesRouter.delete("/clear", deleteAllNotes)
+
+notesRouter.delete("/:id", deleteNote)
+
+notesRouter.put("/:id", updateNote)
 
 module.exports = notesRouter
